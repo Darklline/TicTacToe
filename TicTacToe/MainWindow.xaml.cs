97b6890af940e4c8b7cd01d763c5ba706060f5d2
Darklline <ivaxy12@gmail.com>
2020-07-03 15:23:24 +0200

    CheckForWinner function

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -85,6 +85,95 @@ namespace TicTacToe
             if (!mPlayer1Turn) button.Foreground = Brushes.Red;
             //Change players turns
             mPlayer1Turn ^= true;
+
+            CheckForWinner();
+        }
+
+        private void CheckForWinner()
+        {
+                #region Horizontal Wins
+                //  - Row 0
+                if (mResults[0] != MarkType.Free && (mResults[0] & mResults[1] & mResults[2]) == mResults[0])
+                {
+                    mGameEnded = true;
+
+                    Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.Green;
+                }
+                //  - Row 1
+                if (mResults[3] != MarkType.Free && (mResults[3] & mResults[4] & mResults[5]) == mResults[3])
+                {
+                    mGameEnded = true;
+
+                    Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.Green;
+                }
+                //  - Row 2
+                if (mResults[6] != MarkType.Free && (mResults[6] & mResults[7] & mResults[8]) == mResults[6])
+                {
+                    mGameEnded = true;
+
+                    Button0_2.Background = Button1_2.Background = Button2_2.Background = Brushes.Green;
+                }
+
+                #endregion
+
+                #region Vertical Wins
+                //  - Column 0
+                if (mResults[0] != MarkType.Free && (mResults[0] & mResults[3] & mResults[6]) == mResults[0])
+                {
+                    mGameEnded = true;
+
+                    Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Green;
+                }
+                //  - Column 1
+                if (mResults[1] != MarkType.Free && (mResults[1] & mResults[4] & mResults[7]) == mResults[1])
+                {
+                    mGameEnded = true;
+
+                    Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Green;
+                }
+                //  - Column 2
+                if (mResults[2] != MarkType.Free && (mResults[2] & mResults[5] & mResults[8]) == mResults[2])
+                {
+                    mGameEnded = true;
+
+                    Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Green;
+                }
+
+            #endregion
+
+            #region Diagonally Wins
+            //   Win '\'
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[4] & mResults[8]) == mResults[0])
+                {
+                    mGameEnded = true;
+
+                    Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.Green;
+                }
+                //   Win '/'
+                if (mResults[2] != MarkType.Free && (mResults[2] & mResults[4] & mResults[6]) == mResults[2])
+                {
+                    mGameEnded = true;
+
+                    Button2_0.Background = Button1_1.Background = Button0_2.Background = Brushes.Green;
+                }
+
+                #endregion
+
+                #region No Winners
+
+                if (!mResults.Any(f => f == MarkType.Free))
+                {
+                    mGameEnded = true;
+
+                    Container.Children.Cast<Button>().ToList().ForEach(button =>
+                    {
+                        button.Background = Brushes.Black;
+                    });
+                }
+
+                #endregion
+            }
+
         }
     }
-}
+
